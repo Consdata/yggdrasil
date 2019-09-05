e239d1f3f3bffbda704f70a627250a3f9489e48c
Marcin Mendlik <mmendlik@consdata.pl>
2019-09-05 21:38:37 +0200

    id for MockTreeNode

--- yggdrasil-skill-browser-rest/src/main/java/pl/consdata/yggdrasil/skillbrowser/tree/MockTree.java
+++ yggdrasil-skill-browser-rest/src/main/java/pl/consdata/yggdrasil/skillbrowser/tree/MockTree.java
@@ -10,74 +10,75 @@ class MockTree {
         result.setTreeTitle("Software Developer");
 
         List<TreeNode> nodes = Arrays.asList(
-                TreeNode.builder().title("Company Man")
+                TreeNode.builder().title("Company Man").id("6")
                         .childNodes(
                         Arrays.asList(
-                                TreeNode.builder().title("OpenSource")
+                                TreeNode.builder().title("OpenSource").id("7")
                                 .childNodes(Arrays.asList(
-                                        TreeNode.builder().title("Heimdall").build(),
-                                        TreeNode.builder().title("Kafka Companion").build(),
-                                        TreeNode.builder().title("SonarQube Companion").build(),
-                                        TreeNode.builder().title("tsling-config").build(),
-                                        TreeNode.builder().title("sasslint-config").build()
+                                        TreeNode.builder().title("Heimdall").id("1").build(),
+                                        TreeNode.builder().title("Kafka Companion").id("2").build(),
+                                        TreeNode.builder().title("SonarQube Companion").id("3").build(),
+                                        TreeNode.builder().title("tsling-config").id("4").build(),
+                                        TreeNode.builder().title("sasslint-config").id("5").build()
                                 ))
                                 .build(),
                                 TreeNode.builder()
-                                        .title("blot.condata.tech")
+                                        .title("blot.condata.tech").id("8")
                                         .childNodes(Arrays.asList(
-                                                TreeNode.builder().title("support").build(),
-                                                TreeNode.builder().title("posts").build()
+                                                TreeNode.builder().title("support").id("9").build(),
+                                                TreeNode.builder().title("posts").id("10").build()
                                         ))
                                         .build(),
                                 TreeNode.builder()
-                                        .title("Consdathon")
+                                        .title("Consdathon").id("11")
                                         .childNodes(Arrays.asList(
-                                                TreeNode.builder().title("organiser").build(),
-                                                TreeNode.builder().title("paricipant").build()
+                                                TreeNode.builder().title("organiser").id("12").build(),
+                                                TreeNode.builder().title("paricipant").id("13").build()
                                         ))
                                         .build(),
                                 TreeNode.builder()
                                         .title("Konferencje")
+                                        .id("14")
                                         .childNodes(Arrays.asList(
-                                                TreeNode.builder().title("wystapienia").build()
+                                                TreeNode.builder().title("wystapienia").id("15").build()
                                         ))
                                         .build()
                         )
                 ).build()
-                , TreeNode.builder().title("temp")
+                , TreeNode.builder().title("temp").id("16")
                         .childNodes(
                                 Arrays.asList(
-                                        TreeNode.builder().title("znajomość osx")
+                                        TreeNode.builder().title("znajomość osx").id("17")
                                                 .build(),
                                         TreeNode.builder()
-                                                .title("znajomość windows")
+                                                .title("znajomość windows").id("18")
                                                 .build(),
                                         TreeNode.builder()
-                                                .title("ppliszko")
+                                                .title("ppliszko").id("19")
                                                 .childNodes(Arrays.asList(
                                                         TreeNode.builder()
-                                                                .title("Frontend")
+                                                                .title("Frontend").id("20")
                                                                 .childNodes(Arrays.asList(
                                                                         TreeNode.builder()
-                                                                                .title("HTML")
+                                                                                .title("HTML").id("21")
                                                                                 .childNodes(Arrays.asList(
-                                                                                        TreeNode.builder().title("HTML5").build(),
-                                                                                        TreeNode.builder().title("znajomość standardu").build(),
-                                                                                        TreeNode.builder().title("semantyczny html").build(),
-                                                                                        TreeNode.builder().title("tagi i atrybuty").build(),
-                                                                                        TreeNode.builder().title("wsparcie przeglądarek").build()
+                                                                                        TreeNode.builder().title("HTML5").id("22").build(),
+                                                                                        TreeNode.builder().title("znajomość standardu").id("23").build(),
+                                                                                        TreeNode.builder().title("semantyczny html").id("24").build(),
+                                                                                        TreeNode.builder().title("tagi i atrybuty").id("25").build(),
+                                                                                        TreeNode.builder().title("wsparcie przeglądarek").id("26").build()
                                                                                 ))
                                                                                 .build(),
                                                                         TreeNode.builder()
-                                                                                .title("stacking context")
+                                                                                .title("stacking context").id("27")
                                                                                 .build(),
                                                                         TreeNode.builder()
-                                                                                .title("web components")
+                                                                                .title("web components").id("28")
                                                                                 .childNodes(Arrays.asList(
-                                                                                        TreeNode.builder().title("custom elements").build(),
-                                                                                        TreeNode.builder().title("shadow dom").build(),
-                                                                                        TreeNode.builder().title("polymer").build(),
-                                                                                        TreeNode.builder().title("html templates").build()
+                                                                                        TreeNode.builder().title("custom elements").id("29").build(),
+                                                                                        TreeNode.builder().title("shadow dom").id("30").build(),
+                                                                                        TreeNode.builder().title("polymer").id("31").build(),
+                                                                                        TreeNode.builder().title("html templates").id("32").build()
                                                                                 )).build()
 
                                                                 ))
