4ed2224163083dd660d646a1a0bd5432bb6cdf36
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2019-09-05 17:49:28 +0200

    adds placeholder components for amcharts & d3.js

--- yggdrasil-skill-browser-web/src/app/app.module.ts
+++ yggdrasil-skill-browser-web/src/app/app.module.ts
@@ -1,13 +1,18 @@
+import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {SkillBrowserComponent} from './skill-browser/skill-browser.component';
+import { SkillBrowserD3Component } from './skill-browser-d3/skill-browser-d3.component';
+import { SkillBrowserAmchartsComponent } from './skill-browser-amchart/skill-browser-amcharts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SkillBrowserComponent
+    SkillBrowserComponent,
+    SkillBrowserD3Component,
+    SkillBrowserAmchartsComponent
   ],
   imports: [
     BrowserModule,
--- yggdrasil-skill-browser-web/src/app/skill-browser/skill-browser.component.ts
+++ yggdrasil-skill-browser-web/src/app/skill-browser/skill-browser.component.ts
@@ -6,7 +6,18 @@ import {SkillTreeService} from '../skill-tree/skill-tree.service';
 @Component({
   selector: 'yg-skill-browser',
   template: `
-      <pre>{{ tree$ | async | json }}</pre>
+      <div class="switch-view">
+          <button (click)="view = 'blank'">hide</button>
+          <button (click)="view = 'json'">json</button>
+          <button (click)="view = 'd3'">d3</button>
+          <button (click)="view = 'amcharts'">amcharts</button>
+      </div>
+      <div *ngIf="view === 'json'">
+          <b>amcharts</b>
+          <pre>{{ tree$ | async | json }}</pre>
+      </div>
+      <yg-skill-browser-d3 [tree]="tree$ | async" *ngIf="view === 'd3'"></yg-skill-browser-d3>
+      <yg-skill-browser-amcharts [tree]="tree$ | async" *ngIf="view === 'amcharts'"></yg-skill-browser-amcharts>
   `,
   styleUrls: ['./skill-browser.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -14,6 +25,7 @@ import {SkillTreeService} from '../skill-tree/skill-tree.service';
 export class SkillBrowserComponent implements OnInit {
 
   tree$: Observable<SkillTree>;
+  view: 'json' | 'amcharts' | 'd3' | 'blank' = 'blank';
 
   constructor(private service: SkillTreeService) {
   }
