9de3bd659f8acdf2444af64c45ba93ef10acdac7
ksolawa <ksolawa@consdata.pl>
2019-09-05 18:29:38 +0200

    amcharts - poc

--- yggdrasil-skill-browser-web/src/app/app.component.ts
+++ yggdrasil-skill-browser-web/src/app/app.component.ts
@@ -3,7 +3,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 @Component({
   selector: 'yg-root',
   template: `
-      <yg-skill-browser></yg-skill-browser>
+    <yg-skill-browser></yg-skill-browser>
   `,
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
