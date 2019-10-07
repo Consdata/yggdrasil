d748b1def93d9af7c9f7adb5617f78084d4b4462
Grzegorz Lipecki <glipecki@consdata.pl>
2019-10-07 09:31:31 +0200

    adds logger

--- yggdrasil-skill-browser-web/src/main.ts
+++ yggdrasil-skill-browser-web/src/main.ts
@@ -4,6 +4,13 @@ import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
+import {LoggerFactory, LogLevel} from '@consdata/logger-api';
+import {ConsoleLogAppender} from '@consdata/logger-console';
+import {QueryParamsLogLevelProvider} from '@consdata/logger-web';
+
+LoggerFactory.addAppender(ConsoleLogAppender.instance);
+LoggerFactory.addLogLevelProvider(QueryParamsLogLevelProvider.instance)
+LoggerFactory.setRootLogLevel(LogLevel.INFO);
 
 if (environment.production) {
     enableProdMode();
@@ -13,3 +20,10 @@ am4core.useTheme(amchartsTheme);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
+
+const log = LoggerFactory.getLogger('demo');
+log.info('Tree changed');
+log.debug('Tree changed');
+log.trace('Tree changed');
+log.error('Tree changed');
+
